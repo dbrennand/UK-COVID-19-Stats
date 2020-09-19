c0f9115ca1cd7432cbd55910575cf62e28515221
dbrennand <52419383+dbrennand@users.noreply.github.com>
2020-09-19 14:56:15 +0000

    0.0.1 (#1)
    * Add all project files for 0.0.1
    
    * Add return docstring for get_latest_covid_stats.
    
    * Add step to generate app.secret_key.
    
    * Move secret key to be generated upon starting the app via environment variable.
    
    * Add app.run().
    
    * Add usage.
    
    * Add ability to modify host.
    
    * Add dockerfile.
    
    * Add steps for using with docker.
    
    * Add app image.
    
    * Add data provider.
    
    * Add EOF.

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# COVID19-Stats
-COVID19 Stats Page
+# UK-COVID19-Stats
+
+A simple Flask app showing how the COVID-19 (SARS-CoV-2) pandemic is developing in the UK.
+
+## Dependencies
+
+```pipfile
+[dev-packages]
+autopep8 = "*"
+
+[packages]
+uk-covid19 = "*"
+flask = "*"
+
+[requires]
+python_version = "3.8"
+```
+
+### Dependency installation
+
+`pipenv install` or `pip install -r requirements.txt`
+
+## Usage
+
+### Development
+
+1. Execute the following command to from the project directory to start the app locally:
+
+#### PowerShell
+
+`$Env:SECRET_KEY = $(python -c 'import os; print(os.urandom(16))'); $Env:FLASK_APP = 'app'; $Env:FLASK_ENV = 'development'; flask run`
+
+#### Linux
+
+`export SECRET_KEY=$(python -c 'import os; print(os.urandom(16))'); export FLASK_APP=app; export FLASK_ENV=development; flask run`
+
+### Docker
+
+1. Build the container image: `docker build . --tag uk-covid19-stats:0.0.1 --no-cache`
+
+2. Run the container image: `docker run -e "SECRET_KEY=$(python -c 'import os; print(os.urandom(16))')" -e "FLASK_APP='app'" -e "FLASK_HOST=0.0.0.0" --name uk-covid19-stats --rm -d -p 5000:5000 uk-covid19-stats:0.0.1`
+
+## App image
+
+![UK-COVID19-Stats](uk-covid19-stats.png)
+
+## Data provider
+
+Data is provided by the Public Health England COVID-19 API.
+
+For more information see the [API documentation](https://coronavirus.data.gov.uk/developers-guide).
+
+## Authors -- Contributors
+
+* **dbrennand** - *Author* - [dbrennand](https://github.com/dbrennand)
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) for details.
---
+++ app.py
@@ -0,0 +1,129 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from uk_covid19 import Cov19API
+import os
+
+__version__ = "0.0.1"
+
+# Init Flask app
+app = Flask(__name__)
+
+
+def get_latest_covid_stats():
+    """
+    Obtain current statistics from Public Health Englands COVID-19 API.
+    :return: A Python dictionary object containing the response from the Public Health England API.
+    """
+    # Declare response structure to retrieve
+    structure = {
+        "date": "date",
+        "name": "areaName",
+        "code": "areaCode",
+        "stats": {
+            "cases": {
+                "New cases": {
+                    "value": "newCasesByPublishDate"
+                },
+                "Cumulative cases": {
+                    "value": "cumCasesByPublishDate"
+                },
+                "Male cases": {
+                    "value": "maleCases"
+                },
+                "Female cases": {
+                    "value": "femaleCases"
+                },
+                "Hospital cases": {
+                    "value": "hospitalCases"
+                },
+                "COVID-19 occupied beds with mechanical ventilators": {
+                    "value": "covidOccupiedMVBeds"
+                }
+            },
+            "deaths": {
+                "Daily deaths": {
+                    "value": "newDeaths28DaysByDeathDate",
+                },
+                "Cumulative deaths": {
+                    "value": "cumDeaths28DaysByDeathDate"
+                }
+            },
+            "admissions": {
+                "New admissions": {
+                    "value": "newAdmissions"
+                },
+                "Cumulative admissions": {
+                    "value": "cumAdmissions"
+                }
+            },
+            "tests": {
+                "New pillar one tests by publish date": {
+                    "metadata": "Pillar 1 (NHS and, in England, PHE)",
+                    "value": "newPillarOneTestsByPublishDate"
+                },
+                "Cumulative pillar one tests by publish date": {
+                    "metadata": "Pillar 1 (NHS and, in England, PHE)",
+                    "value": "cumPillarOneTestsByPublishDate"
+                },
+                "New pillar two tests by publish date": {
+                    "metadata": "Pillar 2 (Commercial partners)",
+                    "value": "newPillarTwoTestsByPublishDate"
+                },
+                "Cumulative pillar two tests by publish date": {
+                    "metadata": "Pillar 2 (Commercial partners)",
+                    "value": "cumPillarTwoTestsByPublishDate"
+                },
+                "New pillar three tests by publish date": {
+                    "metadata": "Pillar 3 (Antibody)",
+                    "value": "newPillarThreeTestsByPublishDate"
+                },
+                "Cumulative pillar three tests by publish date": {
+                    "metadata": "Pillar 3 (Antibody)",
+                    "value": "cumPillarThreeTestsByPublishDate"
+                },
+                "New pillar four tests by publish date": {
+                    "metadata": "Pillar 4 (Surveillance)",
+                    "value": "newPillarFourTestsByPublishDate"
+                },
+                "Cumulative pillar four tests by publish date": {
+                    "metadata": "Pillar 4 (Surveillance)",
+                    "value": "cumPillarFourTestsByPublishDate"
+                }
+            }
+        }
+    }
+
+    # Instantiate API object
+    api = Cov19API(
+        filters=["areaType=nation"],
+        structure=structure,
+        latest_by="newCasesByPublishDate"
+    )
+
+    # Obtain JSON from the API
+    response = api.get_json()
+    return response
+
+
+@app.route("/error", methods=["GET"])
+def error():
+    """
+    Error page.
+    """
+    return render_template("error.html")
+
+
+@app.route("/", methods=["GET"])
+def index():
+    """
+    Serve index page.
+    """
+    try:
+        data = get_latest_covid_stats()
+    except:
+        flash("An error occurred obtaining latest COVID-19 stats from the Public Health England API.")
+        return redirect(url_for("error"))
+    return render_template("index.html", data=data)
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("FLASK_HOST", "127.0.0.1"))
