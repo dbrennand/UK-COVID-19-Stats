8a4f2ddb57bb4f341d876fbc10e95b2e8aeca689
dbrennand <52419383+dbrennand@users.noreply.github.com>
2020-09-24 20:41:01 +0000

    0.0.2 (#2)
    * * Bump version to 0.0.2.
    * Add FailedRequestError exception for explicit catching of error when API doesn't return data.
    * Obtain secret key from ENV var or generate a random one.
    * Remove male and female cases. Always blank.
    * Add loguru module for debuging purposes.
    * Add new feature to see COVID-19 related news.
    
    * Add feedparser module and loguru.
    
    * Add navbar and favicon.
    
    * * Add favicon.
    * Add responsive navbar.
    * Fix layout to be responsive on mobile devices.
    
    * Add new news page.
    
    * Update README with new image of news page.
    Add new step for using with docker.
    
    * Update and add new image.
    
    * Fix port number.
    
    * Fix image reference.
    
    * Add EOF.
    
    * Fix docstring for functions.

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from uk_covid19 import Cov19API
+from uk_covid19.exceptions import FailedRequestError
 import os
+from feedparser import parse
+from re import findall, IGNORECASE
+# Logging and debug imports
+from loguru import logger
+from pprint import pprint
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 # Init Flask app
 app = Flask(__name__)
+# Set application secret key from environment variable
+# If environment is not present, generate a random secret key
+app.secret_key = os.environ.get("SECRET_KEY", os.urandom(16))
 
 
 def get_latest_covid_stats():
@@ -25,12 +34,6 @@ def get_latest_covid_stats():
                 },
                 "Cumulative cases": {
                     "value": "cumCasesByPublishDate"
-                },
-                "Male cases": {
-                    "value": "maleCases"
-                },
-                "Female cases": {
-                    "value": "femaleCases"
                 },
                 "Hospital cases": {
                     "value": "hospitalCases"
@@ -99,17 +102,48 @@ def get_latest_covid_stats():
         latest_by="newCasesByPublishDate"
     )
 
+    logger.debug(
+        "Attempting to make request to the Public Health England COVID-19 API.")
     # Obtain JSON from the API
     response = api.get_json()
+    # Log JSON response to logger
+    logger.debug(pprint(response))
     return response
+
+
+def get_health_feed():
+    """
+    Parse BBC news health feed and remove articles not related to COVID-19.
+    """
+    feed = parse("http://feeds.bbci.co.uk/news/health/rss.xml")
+    # log parsed feed for debugging purposes
+    logger.debug(pprint(feed.entries))
+    logger.debug(f"Feed items before removal: {len(feed.entries)}.")
+    # Remove all feed items not related to COVID-19
+    for index, article in enumerate(feed.entries):
+        if any(findall(r'Coronavirus|COVID|Covid|Covid-19', article.title, IGNORECASE)):
+            continue
+        else:
+            logger.debug(f"Removing item at index: {index}.")
+            feed.entries.pop(index)
+    return feed
 
 
 @app.route("/error", methods=["GET"])
 def error():
     """
-    Error page.
+    Serve error page.
     """
     return render_template("error.html")
+
+
+@app.route("/news", methods=["GET"])
+def news():
+    """
+    Serve health page containing news related to COVID-19.
+    """
+    feed = get_health_feed()
+    return render_template("news.html", feed=feed)
 
 
 @app.route("/", methods=["GET"])
@@ -119,7 +153,10 @@ def index():
     """
     try:
         data = get_latest_covid_stats()
-    except:
+    except FailedRequestError as err:
+        # Log error response to logger
+        logger.debug(
+            f"Request to Public Health England COVID-19 API failed: {err}.")
         flash("An error occurred obtaining latest COVID-19 stats from the Public Health England API.")
         return redirect(url_for("error"))
     return render_template("index.html", data=data)
